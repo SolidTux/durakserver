ba58ee1c615272f486cad39e32b9ddd2836f1bae
Daniel Hauck <mail@dhauck.eu>
2017-09-30 23:23:07 +0200

    Complete restart.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-extern crate rand;
-
-mod game;
-mod server;
-
-use server::*;
-
-fn main() {
-    let mut server = Server::new("0.0.0.0:2342").unwrap();
-    server.run().unwrap();
-}
+fn main() {}
