1e216e8c6a4154a229884f42d3823fe9779d7fa3
Daniel Hauck <mail@dhauck.eu>
2017-10-16 14:07:15 +0200

    Add codecov.io support.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,22 @@ extern crate rand;
 pub mod network;
 pub mod game;
 pub mod rules;
+
+#[cfg(test)]
+mod tests {
+    use game::*;
+
+    #[test]
+    fn card_compare_same_suite() {
+        assert_eq!(
+            Some(true),
+            Card::new(CardValue::Ace, Suite::Diamonds).better_as(
+                Card::new(
+                    CardValue::Number7,
+                    Suite::Diamonds,
+                ),
+                Suite::Clubs,
+            )
+        )
+    }
+}
