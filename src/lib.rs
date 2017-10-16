3b436db764e241204e8b2378ce32e06a0a03fad3
Daniel Hauck <mail@dhauck.eu>
2017-10-16 14:28:47 +0200

    More tests for card.

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,55 @@ mod tests {
                 ),
                 Suite::Clubs,
             )
-        )
+        );
+        assert_eq!(
+            Some(false),
+            Card::new(CardValue::Number10, Suite::Diamonds).better_as(
+                Card::new(CardValue::King, Suite::Diamonds),
+                Suite::Diamonds,
+            )
+        );
+    }
+
+    #[test]
+    fn card_compare_trump() {
+        assert_eq!(
+            Some(true),
+            Card::new(CardValue::Number10, Suite::Diamonds).better_as(
+                Card::new(CardValue::King, Suite::Hearts),
+                Suite::Diamonds,
+            )
+        );
+        assert_eq!(
+            Some(false),
+            Card::new(CardValue::Number10, Suite::Hearts).better_as(
+                Card::new(
+                    CardValue::King,
+                    Suite::Diamonds,
+                ),
+                Suite::Diamonds,
+            )
+        );
+    }
+
+    #[test]
+    fn card_compare_fail() {
+        assert_eq!(
+            None,
+            Card::new(CardValue::Number10, Suite::Hearts).better_as(
+                Card::new(
+                    CardValue::King,
+                    Suite::Diamonds,
+                ),
+                Suite::Spades,
+            )
+        );
+        assert_eq!(
+            None,
+            Card::new(CardValue::Jack, Suite::Hearts).better_as(
+                Card::new(CardValue::Number7, Suite::Diamonds),
+                Suite::Spades,
+            )
+        );
     }
 }
